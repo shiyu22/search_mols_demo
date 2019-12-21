b7ed864b3448337fd791b599dc48f5ac36111473
shiyu22 <cshiyu22@gmail.com>
2019-12-21 13:10:44 +0800

    update

--- src/service/search.py
+++ src/service/search.py
@@ -9,6 +9,7 @@ from diskcache import Cache
 def query_name_from_ids(vids):
     res = []
     cache = Cache(default_cache_dir)
+    print(cache)
     for i in vids:
         if i in cache:
             res.append(cache[i])
