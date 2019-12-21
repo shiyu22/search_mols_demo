0593b1bb4eec1502c32d4a95b458f9e6fd1de4e7
shiyu22 <cshiyu22@gmail.com>
2019-12-21 16:39:06 +0800

    update

--- src/encoder/encode.py
+++ src/encoder/encode.py
@@ -60,6 +60,6 @@ def feature_extract(table_name, filepath):
                 names.append(line)
             except:
                 continue
-            print("---------------------------------"names,feats)
+            print("---------------------------------",names,feats)
             print ("extracting feature from smi No. %d , %d images in total" %(current, total))
     return feats, names
