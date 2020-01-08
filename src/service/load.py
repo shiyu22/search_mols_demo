2875c5b12a97e95b1fcf93494af2ff93eb5a005d
shiyu22 <cshiyu22@gmail.com>
2020-01-08 10:01:34 +0800

    update

--- src/service/load.py
+++ src/service/load.py
@@ -21,8 +21,16 @@ def do_load(table_name, database_path):
             print("create table.")
             create_table(index_client, table_name=table_name)
         print("insert into:", table_name)
-        status, ids = insert_vectors(index_client, table_name, vectors)
-        print(status)
+
+        # status, ids = insert_vectors(index_client, table_name, vectors)
+        while ids_lens<len(vectors) :
+            try:
+                status, ids = insert_vectors(index_client, table_name, vectors[ids_lens:ids_lens+100000])
+            except:
+                status, ids = insert_vectors(index_client, table_name, vectors[ids_lens:len(vectors)])
+            ids_lens += 100000
+            print("ids:",len(ids))
+
         create_index(index_client, table_name)
         for i in range(len(names)):
             cache[ids[i]] = names[i]
